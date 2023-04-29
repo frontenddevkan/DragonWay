48fb12e4e2c3acad8b7ffd26159f1cd049e23ee0
Frontenddevkan <frontenddevkan@gmail.com>
2023-04-29 14:20:21 +0500

    feat: practise in javascript

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 "use strict"
 
+function rememberMyFilms() {
+    for (let j = 0; j < 2; j++) {
+        const a = prompt('One of the lastest movies?', '');
+        b = prompt('How do you cost it?', '');
 
-function exampleCode(argumment) {
-    console.log(argumment);
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
 
-exampleCode('what?');
+rememberMyFilms();
+
+
+function showMyDB (privat) {
+    if (privat === false) {
+        console.log("main object");
+    } else {
+
+    }
+}
+
+function whiteYourGenres() {
+    for (let i=0; i < 4; i++) {
+        prompt('Ваш любимый жанр под номером ${')
+    }
+}
